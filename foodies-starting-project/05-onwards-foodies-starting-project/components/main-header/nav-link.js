33aa85aba99972ccdda570d6a6b10abaf8629290
Angrypig123 <shguddnr3@naver.com>
2024-07-30 22:19:03 +0900

    add classes.link class

--- foodies-starting-project/05-onwards-foodies-starting-project/components/main-header/nav-link.js
+++ foodies-starting-project/05-onwards-foodies-starting-project/components/main-header/nav-link.js
@@ -7,7 +7,7 @@ export default function NavLink({href, children}) {
     const path = usePathname();
     return (
         <>
-            <Link href={href} className={path.startsWith(href) ? classes.active : undefined}>
+            <Link href={href} className={path.startsWith(href) ? `${classes.link} ${classes.active}` : `${classes.link}`}>
                 {children}
             </Link>
         </>
